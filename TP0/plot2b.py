6b57185951caa2dd17f9ae19d72d914f70c168fc
Gaston Francois <gfrancois@itba.edu.ar>
2023-08-12 08:27:25 -0300

    add initial 2b

---
+++ TP0/plot2b.py
@@ -0,0 +1,15 @@
+import seaborn
+import pandas
+import sys
+import matplotlib.pyplot as plt
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Please provide the csv input.")
+        sys.exit(1)
+
+    csv = pandas.read_csv(sys.argv[1])
+    res = seaborn.lineplot(data=csv, x="current_hp", y="capture_rate", hue="name", legend="auto").set(title='%HP vs CAPTURE RATE')
+    plt.legend(title='Pokemon')
+    plt.savefig(sys.argv[1].replace('csv', 'png'))
+    plt.show()
