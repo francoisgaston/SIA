ee1d72baec92f1c09a347303c833cb0f24525c27
JoseMenta <jrmenta2@gmail.com>
2023-08-12 21:01:14 -0300

    add plot for general effect in 2a and add labels

--- TP0/plot/2a/plot2a.py
+++ TP0/plot/2a/plot2a.py
@@ -9,35 +9,43 @@ if __name__ == "__main__":
         sys.exit(1)
 
     csv = pd.read_csv(sys.argv[1])
-    # para ver todos los datos
-    with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
-        csv["average"] = csv["count"]/csv["total"]
-        grouped = csv.groupby(["pokemon","status_effect"])
-        print(csv.groupby(["pokemon","status_effect","pokeball"]).sum()) # solo esta para chequear
-        # nos quedamos con el minimo, maximo y media de la proporcion capturada para un pokemon y status_effect dado
-        ans = grouped["average"].agg(["min","max","mean"]).reset_index() # agrega operando sobre la columna indicada
-        ans["max_error"] = ans["max"] - ans["mean"]
-        ans["min_error"] = abs(ans["min"]-ans["mean"])
-        fig = go.Figure()
-        for effect in ["NONE","FREEZE","SLEEP","PARALYSIS","BURN","POISON"]:
-            aux = ans[ans["status_effect"]==effect]
-            print(aux["status_effect"])
-            fig.add_trace(go.Bar(
-                name=effect,
-                x=aux["pokemon"],y=aux["mean"],
-                error_y=dict(
-                    type='data',  # error type: data (default), percent, constant
-                    symmetric=False,  # True (default) for symmetric, False for asymmetric error bars
-                    array=aux["max_error"],  # array of error values (positive direction)
-                    arrayminus=aux["min_error"],  # array of error values (negative direction)
-                    visible=True  # Show the error bars
-                )
-            ))
-        fig.show()
-        print(ans)
-        # fig = go.Figure()
-
-        # fig = px.scatter(ans, x="status_effect", y="mean", color="pokemon")
-        # fig.show()
-
-        #hacer un promedio con los 5 pokemons para cada status, para el punto final
+    csv["percentage"] = csv["count"]/csv["total"]
+    grouped = csv.groupby(["pokemon","status_effect"])
+    # nos quedamos con el minimo, maximo y media de la proporcion capturada para un pokemon y status_effect dado
+    ans = grouped["percentage"].agg(["min","max","mean"]).reset_index() # agrega operando sobre la columna indicada
+    ans["max_error"] = ans["max"] - ans["mean"]
+    ans["min_error"] = abs(ans["min"]-ans["mean"])
+    fig = go.Figure()
+    for effect in ["NONE","FREEZE","SLEEP","PARALYSIS","BURN","POISON"]:
+        aux = ans[ans["status_effect"]==effect]
+        fig.add_trace(go.Bar(
+            name=effect,
+            x=aux["pokemon"],y=aux["mean"],
+            error_y=dict(
+                type='data',
+                symmetric=False,
+                array=aux["max_error"],
+                arrayminus=aux["min_error"],
+                visible=True
+            )
+        ))
+    fig.update_layout(title="Status effect Vs percentage",
+                      xaxis=dict(title="Pokemon"),
+                      yaxis=dict(title="Percentage captured"))
+    fig.show()
+    #Vamos a hacer el resumen de la tendencia general para un status_effect
+    #hacer un promedio con los 5 pokemons para cada status, para el punto final
+    summary = csv.groupby(["status_effect"])
+    summary = summary["percentage"].agg(["min","max","mean"]).reset_index()
+    base = summary[summary["status_effect"]=="NONE"]["mean"].iloc[0]
+    summary["factor"] = summary["mean"]/base
+    print(summary)
+    factors = go.Figure()
+    factors.add_trace(go.Bar(
+        name="hello",
+        x=summary["status_effect"],y=summary["factor"])
+    )
+    factors.update_layout(title="Status effect Vs None",
+                      xaxis=dict(title="Status effect"),
+                      yaxis=dict(title="Factor from None"))
+    factors.show()
