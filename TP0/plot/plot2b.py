a1257719ca1d83ecc564ab8fcc49fdc6f85db785
Gaston Francois <gfrancois@itba.edu.ar>
2023-08-13 03:42:55 -0300

    add plots 2b

--- TP0/plot/plot2b.py
+++ TP0/plot/plot2b.py
@@ -9,7 +9,8 @@ if __name__ == "__main__":
         sys.exit(1)
 
     csv = pandas.read_csv(sys.argv[1])
-    res = seaborn.lineplot(data=csv, x="current_hp", y="capture_rate", hue="name", legend="auto").set(title='%HP vs CAPTURE RATE')
+    res = seaborn.lineplot(data=csv, x="current_hp", y="capture_rate", hue="name", legend="auto", errorbar=("pi", 90)).set(title='%HP vs CAPTURE RATE')
+
     plt.legend(title='Pokemon')
     plt.savefig(sys.argv[1].replace('csv', 'png'))
     plt.show()
