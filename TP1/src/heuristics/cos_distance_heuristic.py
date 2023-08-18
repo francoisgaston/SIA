8a1ea8fcda59be6b31c969d68a37e800536d08d8
Marco4444 <mscilipoti@itba.edu.ar>
2023-08-18 19:20:42 -0300

    worked with cosine distance a lot

--- TP1/src/heuristics/cos_distance_heuristic.py
+++ TP1/src/heuristics/cos_distance_heuristic.py
@@ -2,7 +2,10 @@ import numpy as np
 # from heuristic import Heuristics 
 from data_structures.SokobanState import SokobanState
 
+
+
 class CosineHeuristic():
+    steps = 0
     @staticmethod
     def set_to_vector(box_set, max_rows, max_cols):
         vector = np.ones(max_rows * max_cols)
@@ -17,5 +20,7 @@ class CosineHeuristic():
         final_vector = CosineHeuristic.set_to_vector(state.goal_points, SokobanState.max_rows, SokobanState.max_cols)
 
         similarity = np.dot(initial_vector, final_vector)
-        return similarity / (np.linalg.norm(initial_vector) * np.linalg.norm(final_vector))
+        cosine_distance = similarity / (np.linalg.norm(initial_vector) * np.linalg.norm(final_vector))
+        CosineHeuristic.steps += 1
+        return (1 - cosine_distance) #* CosineHeuristic.steps
 
--- TP1/src/main.py
+++ TP1/src/main.py
@@ -2,10 +2,13 @@ from data_structures.SokobanState import SokobanState
 from algorithm_picker import Algorithm
 from heuristic_picker import Heuristic
 from input import read_input
+import time
 import sys
 import json
 
 if __name__ == "__main__":
+    start_time = time.time()
+
     if len(sys.argv) <= 1:
         print("Por favor ingrese el archivo de configuración")
         exit(1)
@@ -21,16 +24,20 @@ if __name__ == "__main__":
         state = SokobanState(None, 0, boxes_position, player_coord)
         print(state)
 
-        # solution = AStar.solve(state)
         solver = Algorithm.from_string(config["algorithm"])
         heuristic = Heuristic.from_string(config["heuristic"])
         solution = solver.solve(state, heuristic)
+
         if solution.is_valid():
             for curr in solution.build_solution():
                 print(curr)
         else:
             print("There is no solution for the map")
-        print(solution.visited_count)
-        print(solution.end_state.steps)
 
+        end_time = time.time()
+        execution_time = end_time - start_time
+
+        print(f"Visited Count: {solution.visited_count}")
+        print(f"End State Steps: {solution.end_state.steps}")
+        print(f"Time of Execution: {execution_time:.2f} seconds")
 
