33cbd04f2c1039e36c95ad93920135a5a89b6aa0
Axel Preiti Tasat <axel.preitit@gmail.com>
2023-09-07 10:07:41 -0300

    Update selection methods usage and create CSV file inside results ignored directory

--- TP2/src/replace.py
+++ TP2/src/replace.py
@@ -1,20 +1,15 @@
-import random
-import math
+from selection import Selection
 
 
-def traditional_replace(population,  new_population,  replaced_population_size, method_1, method_2, B):
+def traditional_replace(population,  new_population, replaced_population_size, method_1, method_2, B):
     # unir las dos poblaciones
     replaced_population = population + new_population
 
     # Seleccionamos entre la poblacion actual y la poblacion nueva N individuos
-    replaced_1 = method_1.select(replaced_population, math.ceil(B * replaced_population_size))
-    replaced_2 = method_2.select(replaced_population, math.floor((1-B) * replaced_population_size))
-    replaced_population = replaced_1 + replaced_2
-    
-    return replaced_population
+    return Selection.get_both_populations(replaced_population, replaced_population_size, B, method_1, method_2)
 
 
-def sesgo_replace(population,  new_population,  replaced_population_size, method_1, method_2, B):
+def sesgo_replace(population, new_population, replaced_population_size, method_1, method_2, B):
     
     replaced_population = []
     dif = replaced_population_size - len(new_population)
@@ -23,15 +18,10 @@ def sesgo_replace(population,  new_population,  replaced_population_size, method
         # Si N > K, la nueva poblacion va a estar conformado por los K nuevos individuos y los N-K 
         # restantes son seleccionados entre los individuos de la poblacion actual
         replaced_population += new_population
-
-        replaced_1 = method_1.select(population, math.ceil(B * dif))
-        replaced_2 = method_2.select(population, math.floor((1-B) * dif))
-        replaced_population += replaced_1 + replaced_2
+        replaced_population += Selection.get_both_populations(population, dif, B, method_1, method_2)
     elif dif < 0:
         # Si N < K, la nueva poblacion se va a seleccionar entre los K nuevos individuos
-        replaced_1 = method_1.select(new_population, math.ceil(B * dif))
-        replaced_2 = method_2.select(new_population, math.floor((1-B) * dif))
-        replaced_population = replaced_1 + replaced_2
+        replaced_population = Selection.get_both_populations(new_population, replaced_population_size, B, method_1, method_2)
     else:
         # Si N = K, retorna K
         replaced_population = new_population
