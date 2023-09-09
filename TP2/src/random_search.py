136cd4e115ecd7029d081206d9aeb31d5684d1ac
Marco4444 <mscilipoti@itba.edu.ar>
2023-09-09 17:06:41 -0300

    added plot for mutation variation GEN_UNIFORM is best

--- TP2/src/random_search.py
+++ TP2/src/random_search.py
@@ -88,7 +88,7 @@ config_template = {
 
 # Parameter space for random search
 param_space = {
-    "population_0_count": [50, 100, 150, 200, 250, 500, 100],
+    "population_0_count": [50, 100, 150, 200, 225],
     "stop_condition": ["MAX_GENERATIONS", "MAX_TIME", "ACCEPTABLE_SOLUTION", "CHECK_CONTENT", "CHECK_STRUCTURE"],
     "A": [0.1, 0.5, 0.8],
     "B": [0.1, 0.3, 0.5],
@@ -99,29 +99,24 @@ param_space = {
     "mutation_probability": [0.5, 1, 0.9, 0.95],
     "replace": ["TRADICIONAL", "SESGO"],
     "selection_1": {
-        "name": ["DETER_TOURNAMENT","ROULETTE", "UNIVERSAL", "ELITE", "RANKING", "PROBA_TOURNAMENT", "DETER_TOURNAMENT"],
-        "m": [3, 5, 7],
-        "tc": [5, 10],
-        "t0": [25, 50],
-        "c": [0.5, 1]
+        "name": ["ROULETTE", "UNIVERSAL", "ELITE"],
+        "m": [3, 5, 7]
     },
     "selection_2": {
-        "name": ["DETER_TOURNAMENT","ROULETTE", "UNIVERSAL", "ELITE", "RANKING", "PROBA_TOURNAMENT", "DETER_TOURNAMENT"],
-        "m": [3, 5, 7],
+        "name": ["ROULETTE", "UNIVERSAL", "ELITE"],
         "tc": [5, 10],
         "t0": [25, 50],
         "c": [0.5, 1]
+    },
+    "deter_tournament": {
+        "m": [3, 5, 7]
     },
     "replace_1": {
-        "name": ["DETER_TOURNAMENT","ROULETTE", "UNIVERSAL", "ELITE", "RANKING", "PROBA_TOURNAMENT", "DETER_TOURNAMENT"],
-        "m": [3, 5, 7],
-        "tc": [5, 10],
-        "t0": [25, 50],
-        "c": [0.5, 1]
+        "name": ["ELITE"],
+        "m": [3, 5, 7, 11, 15]
     },
     "replace_2": {
-        "name": ["DETER_TOURNAMENT","ROULETTE", "UNIVERSAL", "ELITE", "RANKING", "PROBA_TOURNAMENT", "DETER_TOURNAMENT"],
-        "m": [3, 5, 7],
+        "name": ["BOLTZMANN"],
         "tc": [5, 10],
         "t0": [25, 50],
         "c": [0.5, 1]
@@ -129,41 +124,39 @@ param_space = {
 }
 
 # Initialize search
-max_evals = 100
+max_evals = 50
 start_time = datetime.now()
 best_config = None
 best_performance = float('-inf')
-classes = ["WARRIOR", "ARCHER", "INFILTRATE", "DEFENDER"]
 
-for current_class in classes:
-    # Random search loop
-    for eval_num in range(max_evals):
-        # Generate a random configuration
-        current_config = {"class": current_class}
-        for param, possible_values in param_space.items():
-            if isinstance(possible_values, list):
-                current_config[param] = random.choice(possible_values)
-            elif isinstance(possible_values, dict):
-                current_config[param] = {}
-                for sub_param, sub_possible_values in possible_values.items():
-                    current_config[param][sub_param] = random.choice(sub_possible_values)
+# Random search loop
+for eval_num in range(max_evals):
+    # Generate a random configuration
+    current_config = {}
+    for param, possible_values in param_space.items():
+        if isinstance(possible_values, list):
+            current_config[param] = random.choice(possible_values)
+        elif isinstance(possible_values, dict):
+            current_config[param] = {}
+            for sub_param, sub_possible_values in possible_values.items():
+                current_config[param][sub_param] = random.choice(sub_possible_values)
 
-        # Merge with template
-        merged_config = {**config_template, **current_config}
+    # Merge with template
+    merged_config = {**config_template, **current_config}
 
-        # Evaluate the current configuration
-        current_performance = evaluate_model(merged_config)
+    # Evaluate the current configuration
+    current_performance = evaluate_model(merged_config)
 
-        # Update the best configuration
-        if current_performance > best_performance:
-            best_performance = current_performance
-            best_config = merged_config
+    # Update the best configuration
+    if current_performance > best_performance:
+        best_performance = current_performance
+        best_config = merged_config
 
-        # Stop condition based on time
-        if datetime.now() - start_time > timedelta(minutes=10):
-            break
+    # Stop condition based on time
+    if datetime.now() - start_time > timedelta(minutes=10):
+        break
 
-    print(f"Best performance for {current_class}: {best_performance}")
-    print(f"Best configuration: {json.dumps(best_config, indent=4)}")
-
+# Output the best configuration
+print(f"Best performance: {best_performance}")
+print(f"Best configuration: {json.dumps(best_config, indent=4)}")
 
