8be07fad85583963f24ba5ab093392ca8eeb6307
Axel Preiti Tasat <axel.preitit@gmail.com>
2023-09-24 19:37:16 -0300

    Create hooks for perceptron: - on_epoch: Executed after w is updated - on_min_error: Executed after min_error is updated

--- TP3/src/ej2/main.py
+++ TP3/src/ej2/main.py
@@ -7,7 +7,7 @@ import sys
 from ..error import from_str as error_from_str
 from ..condition import from_str as condition_from_str
 from ..activation import from_str as activation_from_str
-from ..utils.write_csv import write_csv
+from ..utils.write_csv import create_csv, append_row_to_csv
 
 
 # Correr el script desde TP3: pipenv run python -m src.ej2.main src\ej2\config\config.json
@@ -34,7 +34,9 @@ def initialize_w(dim=3, start_random=0, stop_random=1):
 #       En la primera columna de cada dato debe haber un 1 - [[Double]]
 # expected: Es una lista de los valores esperados, expected[i] es el
 #           valor esperado para el i-esimo dato, results[i] - [double]
-def run_perceptron(config, data, expected):
+# on_epoch: función que se ejecuta al actualizar los pesos - (w, epoch) -> void
+# on_min_error: función que se ejecuta al encontrar un nuevo mínimo error - (min_error, epoch) -> void
+def run_perceptron(config, data, expected, on_epoch=None, on_min_error=None):
     i = 0
     if len(data) == 0:
         return []
@@ -48,55 +50,67 @@ def run_perceptron(config, data, expected):
     condition = condition_from_str(config['error'], config['epsilon'])
     error = error_from_str(config['error'], activation_function=activation_function)
 
-    ans = [w.tolist()]
-
     while not condition.check_stop(min_error) and i < config['limit']:
         u = random.randint(0, len(data) - 1)
         h_u = np.dot(data[u], w)
         output_u = activation_function.eval(h_u)
         delta_w = n * (expected[u] - output_u) * activation_function.diff(h_u) * data[u]
         w += delta_w
+        if on_epoch is not None:
+            on_epoch(w, i)
         new_error = error.compute(data, expected, w)
         if condition.check_replace(min_error, new_error):
             min_error = new_error
-            print("min_error", min_error)
+            if on_min_error is not None:
+                on_min_error(min_error, i)
             w_min = w
-        ans.append(w.tolist())
         i += 1
 
-    return ans, w_min
+    return w_min
+
+
+def format_data(df, activation_function):
+    data = []
+    expected = []
+
+    for _, row in df.iterrows():
+        auxdata = []
+        dim = len(row) - 1  # -1 porque tenemos el expected
+        for i in range(1, dim + 1):
+            auxdata.append(row["x" + str(i)])
+        data.append(np.array([1] + auxdata))
+        expected.append(row["y"])
+
+    # Si no es lineal, escalamos los datos de entrada
+    expected = activation_function.scale(expected)
+    return np.array(data), np.array(expected)
 
 
 if __name__ == "__main__":
     with open(f"{sys.argv[1]}", "r") as file:
         config = json.load(file)
-        data = []
-        expected = []
         with open(config['data'], 'r', newline='') as data_file:
             df = pd.read_csv(data_file)
-
-            for _, row in df.iterrows():
-                auxdata = []
-                dim = len(row) - 1  # -1 porque tenemos el expected
-                for i in range(1, dim + 1):
-                    auxdata.append(row["x" + str(i)])
-                data.append(np.array([1] + auxdata))
-                expected.append(row["y"])
-
         activation_function = activation_from_str(string=config['activation'], beta=config["beta"])
-        # Si no es lineal, escalamos los datos de entrada
-        expected = activation_function.scale(expected)
-        ans, last = run_perceptron(
-            config=config,
-            data=data,
-            expected=expected,
-        )
+        data, expected = format_data(df, activation_function)
 
         filename = config['output'] + config['data'].split("/")[-1].split(".")[0]
         headers = ["Id"]
-        for i in range(len(ans[0])):
+        for i in range(len(data[0])):
             headers.append("w" + str(i))
-        for i in range(len(ans)):
-            # Los np.arrays son inmutables
-            ans[i] = np.insert(ans[i], 0, i)
-        write_csv(filename, headers, ans)
+        CSV = create_csv(filename, headers)
+
+        def on_epoch(w, epoch):
+            row = [epoch] + w.tolist()
+            append_row_to_csv(CSV, row)
+
+        def on_min_error(min_error, _):
+            print("Nuevo mínimo error: ", min_error)
+
+        w_min = run_perceptron(
+            config=config,
+            data=data,
+            expected=expected,
+            on_epoch=on_epoch,
+            on_min_error=on_min_error
+        )
--- TP3/src/utils/Function.py
+++ TP3/src/utils/Function.py
@@ -7,3 +7,7 @@ class Function:
     # Evaluates the derivative of the function
     def diff(self, x):
         pass
+
+    # Scales the data
+    def scale(self, data):
+        pass
