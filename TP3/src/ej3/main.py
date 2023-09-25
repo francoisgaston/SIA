acbc162eb64090117c9860e5b6a8fae45b682ee5
Axel Preiti Tasat <axel.preitit@gmail.com>
2023-09-25 19:21:19 -0300

    Move training and testing logic outside the perceptron training

--- TP3/src/ej3/main.py
+++ TP3/src/ej3/main.py
@@ -1,15 +1,17 @@
-import csv  
+import csv
 import datetime
 import json
 import numpy as np
 import sys
+import random
+import os
+
 from condition import from_str as condition_from_str
 from activation import from_str as activation_from_str
 from error import from_str as error_from_str
 from multilayerPerceptron import MultiLayerPerceptron
-import random
-import os
-import numpy as np
+
+
 
 # Recibe la data y lo transforma en np's arrays de cada numero
 def read_input(file, input_length):
@@ -17,6 +19,7 @@ def read_input(file, input_length):
     result = [(1 if character == '1' else 0) for character in file1.read().split()]
     result = np.array_split(result, len(result) / input_length)
     return result
+
 
 def split_data(data, expected, test_pct):
     # Shuffle and partition data into training and test sets
@@ -29,17 +32,17 @@ def split_data(data, expected, test_pct):
 
     train_data = [data[i] for i in train_indices]
     train_expected = [expected[i] for i in train_indices]
-    
+
     test_data = [data[i] for i in test_indices]
     test_expected = [expected[i] for i in test_indices]
 
     return train_data, train_expected, test_data, test_expected
 
-def train_perceptron(config, mlp, data, expected, perceptrons_per_layer, on_epoch=None, on_min_error=None):
+
+def train_perceptron(config, mlp, data, expected, on_epoch=None, on_min_error=None):
     if len(data) == 0:
         return []
 
-    train_data, train_expected, test_data, test_expected = split_data(data, expected, config["test_pct"])
     i = 0
     min_error = sys.float_info.max
     n = config['n']
@@ -47,30 +50,28 @@ def train_perceptron(config, mlp, data, expected, perceptrons_per_layer, on_epoc
     condition = condition_from_str(config['error'], config['epsilon'])
     error = error_from_str(config["error"])
     limit = config["limit"]
+    perceptrons_per_layer = config["perceptrons_for_layers"]
 
-    batch = config["batch"] if config["batch"] <= len(train_data) else len(train_data)
+    batch = config["batch"] if config["batch"] <= len(data) else len(data)
 
     while not condition.check_stop(min_error) and i < limit:
         final_delta_w = [np.zeros((perceptrons_per_layer[indx], perceptrons_per_layer[indx - 1] + 1)) for indx in
                          range(len(perceptrons_per_layer) - 1, 0, -1)]
-        u_arr = random.sample(range(len(train_data)), batch)
+        u_arr = random.sample(range(len(data)), batch)
 
         for u in u_arr:
-            values = mlp.forward(train_data[u])
-            aux_error = np.array(train_expected[u]) - np.array(values)
-            deltas = mlp.backward(aux_error, train_data[u], n)
+            values = mlp.forward(data[u])
+            aux_error = np.array(expected[u]) - np.array(values)
+            deltas = mlp.backward(aux_error, data[u], n)
             for aux in range(len(final_delta_w)):
                 final_delta_w[aux] += deltas[aux]
 
         mlp.apply_delta_w(final_delta_w)
 
-        new_error = error.compute(train_data, mlp, train_expected)
-
-        # Calculate test error
-        test_error = error.compute(test_data, mlp, test_expected)
+        new_error = error.compute(data, mlp, expected)
 
         if on_epoch is not None:
-            on_epoch(i, mlp, new_error, test_error)
+            on_epoch(i, mlp)
 
         if condition.check_replace(min_error, new_error):
             if on_min_error is not None:
@@ -92,7 +93,6 @@ if __name__ == "__main__":
     if not os.path.exists(folder_name):
         os.makedirs(folder_name)
 
-
     current_time = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
     # Create the filename
     csv_filename = f"{folder_name}/training_data_{current_time}.csv"
@@ -107,32 +107,39 @@ if __name__ == "__main__":
         # Write the headers for the CSV file only if the file is new
         if not file_exists:
             csv_writer.writerow(['config_id', 'epoca', 'error_training', 'error_test', 'entrada', 'salida',
-                                'capas_ocultas', 'activacion', 'eta', 'beta', 'activation', 'error_function', 'batch'])
+                                 'capas_ocultas', 'activacion', 'eta', 'beta', 'activation', 'error_function', 'batch'])
 
         for config_id, config_file_path in enumerate(sys.argv[1:]):
             with open(config_file_path, "r") as config_file:
                 config = json.load(config_file)
-                
+
                 expected = np.array(config['expected'])
                 data = read_input(config['input'], config['input_length'])  # 
                 activation_function = activation_from_str(string=config['activation'], beta=config["beta"])  # 
-                mlp = MultiLayerPerceptron(config['perceptrons_for_layers'], activation_function)  # 
-                
-                def on_epoch(epoch, mlp, training_error, test_error):
-                    csv_writer.writerow([config_id, epoch, training_error, test_error, config['input'], config['input_length'],
-                                         config['perceptrons_for_layers'], config['activation'], config['n'],
-                                         config['beta'], config['activation'], config["error"], config["batch"]])
+                error = error_from_str(config["error"])
+                mlp = MultiLayerPerceptron(config['perceptrons_for_layers'], activation_function)  #
+                train_data, train_expected, test_data, test_expected = split_data(data, expected, config["test_pct"])
+
+
+                def on_epoch(epoch, mlp):
+                    training_error = error.compute(train_data, mlp, train_expected)
+                    test_error = error.compute(test_data, mlp, test_expected)
+                    csv_writer.writerow(
+                        [config_id, epoch, training_error, test_error, config['input'], config['input_length'],
+                         config['perceptrons_for_layers'], config['activation'], config['n'],
+                         config['beta'], config['activation'], config["error"], config["batch"]])
+
 
                 def on_min_error(epoch, mlp, min_error):
                     print("min_error: ", min_error)
 
-                train_perceptron(config, mlp, data, expected, config['perceptrons_for_layers'], on_epoch, on_min_error)  # 
+
+                train_perceptron(config, mlp, train_data, train_expected, on_epoch, on_min_error)
 
             for weights in mlp.get_all_weights():  # 
                 print(weights)
 
             for i in range(len(data)):
                 print("expected: ", expected[i])
-                obtained = mlp.forward(data[i])  
+                obtained = mlp.forward(data[i])
                 print("obtained: ", obtained)
-
