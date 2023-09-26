5bcda0be412881ac44975e12bbfdc06014bc5f01
Marco4444 <mscilipoti@itba.edu.ar>
2023-09-26 18:32:46 -0300

    added sketch plotter different configs

--- TP3/src/ej3/plotter.py
+++ TP3/src/ej3/plotter.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import plotly.express as px
 import plotly.graph_objects as go
 import sys
 
@@ -9,50 +8,43 @@ def graficar_errores_de_csv(ruta_csv):
     
     # Obtener IDs de configuración únicos
     ids_config_unicos = df['config_id'].unique()
-    
+
+    # Inicializar figura
+    fig = go.Figure()
+
+    # Loop through unique config_ids
     for config_id in ids_config_unicos:
         # Filtrar el DataFrame basado en config_id
         df_filtrado = df[df['config_id'] == config_id]
-        
-        # Extraer atributos de configuración para la anotación
+
+        # Extraer atributos relevantes
         fila_muestra = df_filtrado.iloc[0]
-        atributos_config = {
-            'Capas Ocultas': fila_muestra['capas_ocultas'],
-            'Activación': fila_muestra['activacion'],
-            'Eta': fila_muestra['eta'],
-            'Beta': fila_muestra['beta'],
-            'Función de Activación': fila_muestra['activation'],
-            'Función de Error': fila_muestra['error_function'],
-            'Tamaño de Lote': fila_muestra['batch'],
-            'Error Gaussiano': fila_muestra['noise_stddev'],
-            'Augmentacion': fila_muestra['data_augmentation']
-        }
-        
-        texto_anotacion = "<br>".join([f"{k}: {v}" for k, v in atributos_config.items()])
-        
+        augmentacion = fila_muestra['data_augmentation']
+        stddev = fila_muestra['noise_stddev']
+
+        # Construir el texto simplificado para la anotación/etiqueta
+        if(augmentacion):
+          texto_anotacion = f"Augmentado"
+        else:
+          texto_anotacion = f"No augmentado"
+
         # Ordenar por época
         df_filtrado = df_filtrado.sort_values('epoca')
-        
-        # Graficar usando Plotly
-        fig = px.line(df_filtrado, x='epoca', y=['error_training', 'error_test'],
-                      labels={'value': 'Error', 'epoca': 'Época'},
-                      title=f"Error de Entrenamiento y Prueba para Config {config_id}")
-        
-        # Actualizar la disposición para hacer el eje x logarítmico
-        fig.update_layout(xaxis_type="log", xaxis_title="Época Logarítmica")
 
-        # Agregar la anotación con atributos de configuración
-        fig.add_annotation(x=0, y=0, xref="paper", yref="paper",
-                           text=texto_anotacion,
-                           showarrow=False,
-                           font=dict(size=10),
-                           bordercolor="black",
-                           borderwidth=1,
-                           borderpad=4,
-                           bgcolor="white",
-                           opacity=0.8)
-        
-        fig.show()
+        # Agregar líneas al gráfico
+        fig.add_trace(go.Scatter(x=df_filtrado['epoca'], y=df_filtrado['error_training'],
+                                 mode='lines', name=f"Training {config_id} ({texto_anotacion})"))
+        fig.add_trace(go.Scatter(x=df_filtrado['epoca'], y=df_filtrado['error_test'],
+                                 mode='lines', name=f"Test {config_id} ({texto_anotacion})"))
+
+    # Actualizar etiquetas y título
+    fig.update_layout(xaxis_title="Época Logarítmica",
+                      yaxis_title="Error",
+                      title="Comparacion Errores con y sin augmentacion de datos",
+                      xaxis_type="log")
+
+    # Mostrar figura
+    fig.show()
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
