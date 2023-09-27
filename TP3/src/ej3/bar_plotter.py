21e237fa8158d1a16c29db8acd9f2789b2d6fd4b
Marco4444 <mscilipoti@itba.edu.ar>
2023-09-27 10:48:01 -0300

    added bar_plotter

--- TP3/src/ej3/bar_plotter.py
+++ TP3/src/ej3/bar_plotter.py
@@ -7,15 +7,17 @@ def bar_plot(csv_filename):
     df = pd.read_csv(csv_filename)
 
     # Create a column 'perceptrons_for_layers' that combines 'entrada', 'capas_ocultas', and 'salida'
-    df['perceptrons_for_layers'] = df['entrada'].astype(str) + '-' + \
-                                   df['capas_ocultas'].astype(str) + '-' + \
-                                   df['salida'].astype(str)
+    df['perceptrons_for_layers'] =  df['capas_ocultas'].astype(str)
+    
+    # Find the minimum 'error_training' and 'error_test' for each iteration within each 'config_id'
+    min_errors_df = df.groupby(['config_id', 'iteration', 'perceptrons_for_layers']).agg({'error_training': 'min', 'error_test': 'min'}).reset_index()
 
-    # Group the data by 'config_id', 'iteration', and 'perceptrons_for_layers' to find the minimum 'error_training' and 'error_test' for each iteration
-    grouped_df = df.groupby(['config_id', 'iteration', 'perceptrons_for_layers']).agg({'error_training': 'min', 'error_test': 'min'}).reset_index()
+    # Compute the mean and standard deviation of those minimum errors across all iterations for each 'config_id'
+    statistics_df = min_errors_df.groupby(['config_id', 'perceptrons_for_layers']).agg({'error_training': ['mean', 'std'], 'error_test': ['mean', 'std']}).reset_index()
 
-    # Calculate the mean and standard deviation of the best (minimum) 'error_training' and 'error_test' across iterations for each 'config_id'
-    statistics_df = grouped_df.groupby(['config_id', 'perceptrons_for_layers']).agg({'error_training': ['mean', 'std'], 'error_test': ['mean', 'std']}).reset_index()
+    # Sort by the length of 'capas_ocultas'
+    statistics_df['sort_key'] = statistics_df['perceptrons_for_layers'].apply(len)
+    statistics_df = statistics_df.sort_values('sort_key').reset_index(drop=True)
 
     # Create the bar plot
     fig = go.Figure()
@@ -31,9 +33,9 @@ def bar_plot(csv_filename):
     # Add Spanish labels
     fig.update_layout(
         title='Mejor Error de Entrenamiento y Prueba para Cada Configuración',
-        xaxis_title='Perceptrones por Capas',
+        xaxis_title='Numero de Capas',
         yaxis_title='Mejor Error Promedio',
-        barmode='group'  # this makes bars appear next to each other
+        barmode='group'
     )
 
     # Show the plot
