b5ca4566fec8ba4f05246d3b940ccda98aca02ea
Gaston Francois <gfrancois@itba.edu.ar>
2023-10-13 16:29:55 -0300

    add biplot plotly migration

--- TP4/src/PCA/main.py
+++ TP4/src/PCA/main.py
@@ -24,70 +24,9 @@ def component_graph(countries, pc1):
                       title="PC1 para cada país")
     fig.show()
 
-def biplot(x, ldngs, names, headers):
-    plt.figure(figsize=(8, 8))
-    plt.scatter(x[:, 0], x[:, 1])
 
-    for i, feature in enumerate(headers):
-        plt.arrow(0, 0, ldngs[0, i], ldngs[1, i], color='r', alpha=0.5)
-        plt.text(ldngs[0, i] * 1.15,
-                 ldngs[1, i] * 1.15,
-                 feature, fontsize=18)
-
-    for i in range(len(names)-1):
-        plt.text(x[i, 0], x[i, 1], f'{names[i]}')
-
-    plt.show()
-
-def biplot_plotly(x):
-    biplot_df = pd.DataFrame(x, columns=['CP1', 'CP2'])
-    fig = px.scatter(biplot_df, x='CP1', y='CP2')
-
-    # Agrega las flechas de las cargas
-    for i in range(L.shape[1]):
-        fig.add_trace(
-            px.scatter(x=[0], y=[0], text=[f'Variable {i + 1}'], name=f'Variable {i + 1}').data[0]
-        )
-        fig.add_shape(
-            x0=0, y0=0, x1=L[0, i], y1=L[1, i],
-            line=dict(color='red', width=2, dash='dot'),
-            xref='x', yref='y'
-        )
-
-
-def main():
-    # lectura de input sin nombres de paises
-    data,names, headers = read_input("data/europe.csv")
-    print(len(data))
-    print(headers)
-    print(headers[1::])
-    # Normalizacion de los valores
-    scaling = StandardScaler()
-    scaling.fit(data)
-    Scaled_data = scaling.transform(data)
-    print(Scaled_data)
-
-    # Analisis de PCA
-    principal = PCA(n_components=2)
-    principal.fit(Scaled_data)
-    x = principal.transform(Scaled_data)
-    print(x)
-
-    print("---------")
-    print(sum(principal.explained_variance_ratio_))
-
-    # TODO: revisar si está bien, creo que si porque es como que estoy eligiendo al primer autovector solo
-    pc1 = x[:, 0]
-    component_graph(names, pc1)
-    print(len(names))
-    print(names[0])
-    print(len(pc1))
-    print(len(x))
-    print("---------")
-
+def biplot(principal, x, names, headers):
     loadings = principal.components_.T * np.sqrt(principal.explained_variance_)
-    print(len(names.tolist()))
-    print(len(x))
     fig = px.scatter(x, x=0, y=1, text=names)
 
     for i, feature in enumerate(headers[1::]):
@@ -112,6 +51,29 @@ def main():
             yshift=5,
         )
     fig.show()
+
+
+
+def main():
+    # lectura de input sin nombres de paises
+    data,names, headers = read_input("data/europe.csv")
+
+    # Normalizacion de los valores
+    scaling = StandardScaler()
+    scaling.fit(data)
+    Scaled_data = scaling.transform(data)
+
+    # Analisis de PCA
+    principal = PCA(n_components=2)
+    principal.fit(Scaled_data)
+    x = principal.transform(Scaled_data)
+
+    # TODO: revisar si está bien, creo que si porque es como que estoy eligiendo al primer autovector solo
+    pc1 = x[:, 0]
+    component_graph(names, pc1)
+
+    biplot(principal, x, names, headers)
+
 
 if __name__ == "__main__":
     main()
