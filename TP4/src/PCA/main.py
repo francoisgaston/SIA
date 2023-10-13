1428a5355fc2fd6c7ff7124751be933ce878c4e1
Gaston Francois <gfrancois@itba.edu.ar>
2023-10-13 16:48:14 -0300

    add config

--- TP4/src/PCA/main.py
+++ TP4/src/PCA/main.py
@@ -5,6 +5,8 @@ from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 import plotly.express as px
+import json
+import sys
 
 def read_input(input_file_path):
     df = pd.read_csv(input_file_path)
@@ -60,27 +62,32 @@ def biplot(loadings, PC1, PC2, names, headers):
 
 
 def main():
-    # lectura de input sin nombres de paises
-    data,names, headers = read_input("data/europe.csv")
+    with open(f"{sys.argv[1]}", "r") as file:
+        config = json.load(file)
 
-    # Normalizacion de los valores
-    scaling = StandardScaler()
-    scaling.fit(data)
-    Scaled_data = scaling.transform(data)
+        # lectura de input sin nombres de paises
+        data, names, headers = read_input(config["input"])
 
-    # Analisis de PCA
-    principal = PCA(n_components=2)
-    principal.fit(Scaled_data)
-    x = principal.transform(Scaled_data)
+        # Normalizacion de los valores
+        scaling = StandardScaler()
+        scaling.fit(data)
+        Scaled_data = scaling.transform(data)
 
-    # TODO: revisar si está bien, creo que si porque es como que estoy eligiendo al primer autovector solo
-    pc1 = x[:, 0]
-    component_graph(names, pc1)
+        # Analisis de PCA
+        n_components = config["n_components"]
+        principal = PCA(n_components)
+        principal.fit(Scaled_data)
+        x = principal.transform(Scaled_data)
 
-    # TODO: Preguntar si esta bien la escala de las flechas (Lo saque de sklearn)
-    loadings = principal.components_.T * np.sqrt(principal.explained_variance_)
-    pc2 = x[:, 1]
-    biplot(loadings, pc1, pc2, names, headers[1::])
+        # TODO: revisar si está bien, creo que si porque es como que estoy eligiendo al primer autovector solo
+        pc1 = x[:, 0]
+        component_graph(names, pc1)
+
+        if n_components>1:
+            # TODO: Preguntar si esta bien la escala de las flechas (Lo saque de sklearn)
+            loadings = principal.components_.T * np.sqrt(principal.explained_variance_)
+            pc2 = x[:, 1]
+            biplot(loadings, pc1, pc2, names, headers[1::])
 
 
 if __name__ == "__main__":
