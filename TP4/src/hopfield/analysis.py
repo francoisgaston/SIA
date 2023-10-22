938b8e08effb436eac723e84f20373e726658b18
martinippo01 <84152110+martinippo01@users.noreply.github.com>
2023-10-22 17:34:57 -0300

    Hopfield analyse config

--- TP4/src/hopfield/analysis.py
+++ TP4/src/hopfield/analysis.py
@@ -23,28 +23,29 @@ if __name__ == "__main__":
         input_file = config["input"]
         input_length = config["size"]
         try_file = config["try"]
+        combination_size = config["combination_size"]
 
         patterns = read_input(input_file, input_length)
         letters = {chr(97 + i): patterns[i] for i in range(26)}
         pattern_to_try = read_input(try_file, input_length)
         max_iterations = config["max_iterations"]
 
-        combinations = list(itertools.combinations(letters.keys(), 4))
+        combinations = list(itertools.combinations(letters.keys(), combination_size))
         avg_list = []
         for combination in combinations:
-            print(combination, end="")
+            # print(combination, end="")
             patterns_combination = [np.array(letters[key]) for key in combination]
             max_product, min_product, avg_product = Hopfield._analyse_orthogonality(np.array(patterns_combination))
-            print(" => Max: ", end="")
-            print(max_product, end="")
-            print(" | Min: ", end="")
-            print(min_product, end="")
-            print(" | Avg: ", end="")
-            print(avg_product)
+            # print(" => Max: ", end="")
+            # print(max_product, end="")
+            # print(" | Min: ", end="")
+            # print(min_product, end="")
+            # print(" | Avg: ", end="")
+            # print(avg_product)
             avg_list.append((avg_product, combination))
         df = pd.DataFrame(sorted(avg_list), columns=["|<,>| avg", "combination"])
         # df.head(15).style.format({'|<,>| avg': "{:.2f}"}).hide(axis='index')
-        print(df.head(15))
+        print(df.head(55))
         print(df.tail(15))
 
 
