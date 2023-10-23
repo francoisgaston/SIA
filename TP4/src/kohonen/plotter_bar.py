1ffa4a85733beebcd7f7d5771f95e425e90d5bc6
JoseMenta <jrmenta2@gmail.com>
2023-10-22 21:28:41 -0300

    add bar plotter for kohonen

--- TP4/src/kohonen/plotter_bar.py
+++ TP4/src/kohonen/plotter_bar.py
@@ -11,11 +11,11 @@ def generate_graph(ruta_csv, config):
     grouped_df = df.groupby(config['group_by']).agg(u_mean=("u_matrix_mean",'mean'),u_max=('u_matrix_mean','max'),u_min=("u_matrix_mean",'min'),heatmap_mean=('heatmap_mean','max')).reset_index()
     fig = go.Figure()
 
-    fig.add_trace(go.Scatter(x=grouped_df[config['group_by']], y=grouped_df['u_mean'], error_y=dict(type='data',
+    fig.add_trace(go.Bar(x=grouped_df[config['group_by']], y=grouped_df['u_mean'], error_y=dict(type='data',
             symmetric=False,
             array=grouped_df['u_max'],
             arrayminus=grouped_df['u_min']),name="u_matrix_mean"))
-    fig.add_trace(go.Scatter(x=grouped_df[config['group_by']], y=grouped_df['heatmap_mean'],name="heatmap_mean"))
+
     fig.update_layout(title=config["title"],
     xaxis=dict(title=config['group_by']))
     fig.show()
